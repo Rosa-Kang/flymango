5896fd88b7d2a5e9356639a918fc407e621e23e8
Rosa Kang <therosessom@gmail.com>
2023-08-27 16:08:03 -0700

    update client apiurl to live server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; 
 
-const API = axios.create({baseURL : 'http://localhost:8080'});
+const API = axios.create({baseURL : 'https://small-silence-4346.fly.dev'});
 
 
 API.interceptors.request.use((req) => {
