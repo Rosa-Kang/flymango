1d370d7bb1c5578205d63e886fe774d673f104b8
Rosa Kang <therosessom@gmail.com>
2023-09-10 15:47:58 -0700

    update client

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,17 +3,16 @@ import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionType
 // eslint-disable-next-line
 export default (posts = [], action) => {
   switch (action.type) {
-    case FETCH_ALL:
-      return action.payload;
-    case LIKE:
-      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
-    case CREATE:
-      return [...posts, action.payload];
-    case UPDATE:
-      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
-    case DELETE:
-      return posts.filter((post) => post._id !== action.payload);
-    default:
-      return posts;
+      case FETCH_ALL:
+          return action.payload;
+      case LIKE:
+      case UPDATE:
+          return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+      case CREATE:
+          return [...posts, action.payload];
+      case DELETE:
+          return posts.filter((post) => post._id !== action.payload);
+      default:
+          return posts;
   }
 };
